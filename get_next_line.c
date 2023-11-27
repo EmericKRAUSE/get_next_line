1ceff84e976fa51ed1bc3dc7401c2420c1a782c0
Emeric Krause <ekrause@k1r4p4.42mulhouse.fr>
2023-11-27 17:50:26 +0100

    push

--- get_next_line.c
+++ get_next_line.c
@@ -2,6 +2,15 @@
 
 char    *get_next_line(int fd)
 {
+    char *line;
+    static char *stash;
+    char buff[BUFFER_SIZE + 1];
+    char *temp;
 
+    stash = NULL;
+    if (fd < 0 || BUFFER_SIZE <= 0 || > 1024)
+        return (NULL);
+    
 
+    return (line);
 }
